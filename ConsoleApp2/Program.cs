5104e907448c5bb3acdf4f8f337f80e0d39bbf98
rubysusan <ruby.susan@controlqore.com>
2023-07-04 12:16:25 +0530

    console

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter string data");
+string stringdata = Console.ReadLine();
+
+
+Console.WriteLine("Enter integer data");
+string val2 = Console.ReadLine();
+int.TryParse(val2, out int intdata);
+
+
+Console.WriteLine("Enter character data");
+string val3 = Console.ReadLine();
+char.TryParse(val3, out char chardata);
+
+Console.WriteLine("Enter boolean data");
+string val4 = Console.ReadLine();
+bool.TryParse(val4, out bool booldata);
+
+Console.WriteLine("\nDisplaying Data:");
+Console.WriteLine("String Data : " + stringdata);
+Console.WriteLine("Integer data : " + intdata);
+Console.WriteLine("Character data : " + chardata);
+Console.WriteLine("Boolean data : " + booldata);
+
