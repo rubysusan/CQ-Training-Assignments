f39fe67371638402f56cc4b0b2f195f6e148fd30
rubysusan <ruby.susan@controlqore.com>
2023-07-11 17:04:10 +0530

    student class

---
+++ C# Assignments/Assignment- 11th July/StudentManagement/StudentManagement/Program.cs
@@ -0,0 +1,7 @@
+﻿using StudentManagement;
+Student std = new Student(1, "John", 85);
+std.display();
+Student std1 = new Student(2, "David", 74);
+std1.display();
+Student std2 = new Student(3, "Thomas", 90);
+std2.display();
---
+++ C# Assignments/Assignment- 11th July/StudentManagement/StudentManagement/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public char Grade { get; }
+
+        public Student(int id,string name,int mark)
+        {
+            Id = id;
+            Name = name;
+            Mark = mark;
+            Grade=calcGrade(Mark);
+        }
+        public char calcGrade(int mark)
+        {
+            if (mark >= 90)
+                return 'A';
+            else if (mark >= 80 && mark < 90)
+                return 'B';
+            else if (mark >= 70 && mark < 80)
+                return 'C';
+            else if (mark < 70)
+                return 'D';
+            return 'F';
+        }
+        public void display()
+        {
+            Console.WriteLine("Student Details:");
+            Console.WriteLine("ID : " + Id);
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Mark : " + Mark);
+            Console.WriteLine("Grade : " + Grade);
+        }
+    }
+}
