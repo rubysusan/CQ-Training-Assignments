c3d703668d22d86305c4498aa8ed7ebb66d5580c
rubysusan <ruby.susan@controlqore.com>
2023-07-17 16:39:24 +0530

    question 1 emp class

--- C# Assignments/Assignment- 17th July/ConsoleApp6/ConsoleApp6/Program.cs
+++ C# Assignments/Assignment- 17th July/ConsoleApp6/ConsoleApp6/Program.cs
@@ -1,16 +1,16 @@
 ﻿using ConsoleApp6;
 DateTime dt = DateTime.Now;
-Employee emp= new Employee(111,"RUBY", dt, "HR",15000,'P'); 
+Employee emp= new Employee(111,"RUBY", dt, "HR",15000,'P');
+IBonusCalculator ib = null;
 if(emp.JobType=='P')
 {
-    IBonusCalculator ip = new PermanentEmployeeBonusCalculator();
-    Console.WriteLine("Bonus Amount for Type "+emp.JobType+" Employee =" + ip.CalculateBonus(emp));
+   ib = new PermanentEmployeeBonusCalculator();
 }
 else if (emp.JobType == 'C')
 {
-    IBonusCalculator ic = new ContractEmployeeBonusCalculator();
-    Console.WriteLine("Bonus Amount for Type "+emp.JobType+" Employee =" + ic.CalculateBonus(emp));
-}
+   ib = new ContractEmployeeBonusCalculator();
 
+}
+Console.WriteLine("Bonus Amount for Type " + emp.JobType + " Employee =" + ib.CalculateBonus(emp));
 
 
