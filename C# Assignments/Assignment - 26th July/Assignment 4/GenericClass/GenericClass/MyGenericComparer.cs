b5fb4bc76dbd4ab4886f8549046705283410a62d
rubysusan <ruby.susan@controlqore.com>
2023-07-27 14:51:40 +0530

    Q4

---
+++ C# Assignments/Assignment - 26th July/Assignment 4/GenericClass/GenericClass/MyGenericComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    public class MyGenericComparer<T> where T : IComparable
+    {
+        public T Compare(T x, T y)
+        {
+            if(x.CompareTo(y) > 0)
+                return x;
+            else
+                return y;
+        }
+    }
+}
---
+++ C# Assignments/Assignment - 26th July/Assignment 4/GenericClass/GenericClass/Program.cs
@@ -0,0 +1,7 @@
+﻿using GenericClass;
+MyGenericComparer<int> genInt=new MyGenericComparer<int>();
+int val=genInt.Compare(10, 9);
+Console.WriteLine("Greatest Value = "+val);
+MyGenericComparer<double> genFloat= new MyGenericComparer<double>();
+double val1 = genFloat.Compare(10.9, 9.9);
+Console.WriteLine("Greatest Value = " + val1);
