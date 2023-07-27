daf88f738c9c1bd49d8543bc0cccc35fbeac5f85
rubysusan <ruby.susan@controlqore.com>
2023-07-27 15:36:20 +0530

    Q1

---
+++ C# Assignments/Assignment - 26th July/Assignment 1/EmployeeApp/EmployeeApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using EmployeeModel;
+Employee emp = new Employee("John Doe", 15000, "Permanent");
+Employee emp1 = new Employee("Liam Smith", 20000, "Contract");
+Employee emp2 = new Employee("Mary James", 15000, "Permanent");
+emp.Display();
+emp1.Display();
+emp2.Display();
+emp.EmpCount();
---
+++ C# Assignments/Assignment - 26th July/Assignment 1/EmployeeModel/EmployeeModel/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeeModel
+{
+    public class Employee
+    {
+        static int count;
+        public string Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string EmployeeType { get; set; }
+        static Employee()
+        {
+            count = 1000;
+        }
+        public Employee(string name, double sal, string type)
+        {
+            Id = "EMP" + count++;
+            Name = name;
+            Salary = sal;
+            EmployeeType = type;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Id: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Salary: " + Salary);
+            Console.WriteLine("Employee Type: " + EmployeeType);
+            Console.WriteLine("\n");
+        }
+        public void EmpCount()
+        {
+            Console.WriteLine("Total no. of Employees: " + (count - 1000));
+        }
+    }
+}
