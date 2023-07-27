148c9e45a950c3038583aaa2314c67efa6090a24
rubysusan <ruby.susan@controlqore.com>
2023-07-27 15:09:17 +0530

    Q5

---
+++ C# Assignments/Assignment - 26th July/Assignment 5/ListApp/ListApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.WebSockets;
+
+int[] arrayInt = { 1, 2, 3, 4, 5 };
+List < int> listInt = new List<int>();
+Console.WriteLine("Elements in array: ");
+foreach (int i in arrayInt)
+{
+    Console.Write(i + " ");
+    listInt.Add(i*2);
+}
+Console.WriteLine("\nElements in list: ");
+foreach (int i in listInt)
+{
+    Console.Write(i +" ");
+}
