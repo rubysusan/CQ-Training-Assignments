de03e968fac712c479bbab825be10f5c0004805d
rubysusan <ruby.susan@controlqore.com>
2023-07-27 11:44:59 +0530

    Q2

---
+++ C# Assignments/Assignment - 26th July/Assignment 2/CustomException/CustomException/Program.cs
@@ -0,0 +1,28 @@
+﻿using CustomException;
+char sport;
+Console.WriteLine("Enter the character corresponding to the sports name: "); 
+sport=char.Parse(Console.ReadLine());
+switch(sport)
+{
+    case 'c':
+        Console.WriteLine("Cricket");
+        break;
+    case 'f':
+        Console.WriteLine("Football");
+        break;
+    case 'h':
+        Console.WriteLine("Hockey");
+        break;
+    case 't':
+        Console.WriteLine("Tennis");
+        break;
+    case 'b':
+        Console.WriteLine("Badminton");
+        break;
+    default:
+        try { throw new MyException("Invalid Input"); }
+        catch(MyException e) { Console.WriteLine(e.Message);
+            break;
+        }   
+        
+}
