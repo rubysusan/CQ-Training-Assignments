131f16c2559eea6c89225e775432ea6563e7fc69
rubysusan <ruby.susan@controlqore.com>
2023-08-21 12:32:40 +0530

    forms

--- Angular Assignments/Assignment - 16th August/Employee-Routing - Copy/src/app/Employee/IEmployeeDetail.ts
+++
@@ -1,9 +0,0 @@
-export interface IEmployeeDetail {
-    id: number;
-    firstname : string;
-    lastname : string;
-    dateofbirth : Date;
-    age : number;
-    joiningdate : Date;
-    country : string;
-}
